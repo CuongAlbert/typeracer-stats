4c382fa2e9bc0be28ef8d7cced2aa80b35e15fb1
HoangCoder <93141009+NicolasWillyam@users.noreply.github.com>
2023-07-03 14:10:00 +0700

    Updated

--- apps/next/app/page.tsx
+++ apps/next/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
 import { HomeScreen } from 'app/features/home/screen'
+import { TesingScreen } from 'app/features/home/testingscreen'
 
-export default HomeScreen
+export default { HomeScreen, TesingScreen }
--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,14 +1,20 @@
 import {
   Anchor,
   Button,
+  config,
   H1,
   Paragraph,
   Separator,
   Sheet,
+  Stack,
+  TamaguiProvider,
   useToastController,
   XStack,
   YStack,
 } from '@my/ui'
+import Header from '@my/ui/src/Header'
+import HomeContent from '@my/ui/src/HomeContent'
+import SideBar from '@my/ui/src/SideBar'
 import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
 import React, { useState } from 'react'
 import { useLink } from 'solito/link'
@@ -19,79 +25,16 @@ export function HomeScreen() {
   })
 
   return (
-    <YStack f={1} jc="center" ai="center" p="$4" space>
-      <YStack space="$4" maw={600}>
-        <H1 ta="center">Welcome to Tamagui.</H1>
-        <Paragraph ta="center">
-          Here's a basic starter to show navigating from one screen to another. This screen uses the
-          same code on Next.js and React Native.
-        </Paragraph>
-
-        <Separator />
-        <Paragraph ta="center">
-          Made by{' '}
-          <Anchor color="$color12" href="https://twitter.com/natebirdman" target="_blank">
-            @natebirdman
-          </Anchor>
-          ,{' '}
-          <Anchor
-            color="$color12"
-            href="https://github.com/tamagui/tamagui"
-            target="_blank"
-            rel="noreferrer"
-          >
-            give it a ⭐️
-          </Anchor>
-        </Paragraph>
-      </YStack>
-
-      <XStack>
-        <Button {...linkProps}>Link to user</Button>
-      </XStack>
-
-      <SheetDemo />
-    </YStack>
-  )
-}
-
-function SheetDemo() {
-  const [open, setOpen] = useState(false)
-  const [position, setPosition] = useState(0)
-  const toast = useToastController()
-
-  return (
-    <>
-      <Button
-        size="$6"
-        icon={open ? ChevronDown : ChevronUp}
-        circular
-        onPress={() => setOpen((x) => !x)}
-      />
-      <Sheet
-        modal
-        open={open}
-        onOpenChange={setOpen}
-        snapPoints={[80]}
-        position={position}
-        onPositionChange={setPosition}
-        dismissOnSnapToBottom
-      >
-        <Sheet.Overlay />
-        <Sheet.Frame ai="center" jc="center">
-          <Sheet.Handle />
-          <Button
-            size="$6"
-            circular
-            icon={ChevronDown}
-            onPress={() => {
-              setOpen(false)
-              toast.show('Sheet closed!', {
-                message: 'Just showing how toast works...',
-              })
-            }}
-          />
-        </Sheet.Frame>
-      </Sheet>
-    </>
+    <TamaguiProvider config={config} defaultTheme="light">
+      <Stack>
+        <Header />
+        <XStack display="flex">
+          <SideBar />
+          <Stack w="100%" pl={220} bg={'#13161E'}>
+            <HomeContent />
+          </Stack>
+        </XStack>
+      </Stack>
+    </TamaguiProvider>
   )
 }
